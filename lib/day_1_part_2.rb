3bb7bd7f0992fda30e96221153dc7a06b5d7325e
James Reid-Smith <james.reidsmith@shopify.com>
2024-12-02 08:00:11 -0500

    With tally use for faster tests

--- lib/day_1_part_2.rb
+++ lib/day_1_part_2.rb
@@ -2,7 +2,8 @@ require_relative "day_1_part_1"
 
 class Day1Part2 < Day1Part1
   def similarities
-    left.map { |left_item| left_item * right.count(left_item) }
+    tallies = right.tally
+    left.map { |left_item| left_item * (tallies[left_item] || 0) }
   end
 
   def sum_similarities
